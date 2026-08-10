11f3d2e97980c3ff75e047b9221a5efe705219ee
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-08-10 23:59:26 +0300

    8  Sealed Class

--- OOP_Assignment3/Program.cs
+++ OOP_Assignment3/Program.cs
@@ -256,6 +256,32 @@
         }
     }
 
+    public sealed class CompletedShipment : Shipment
+    {
+        public override string ShipmentType => "Completed Shipment";
+
+        public CompletedShipment(
+            string trackingCode,
+            string description,
+            decimal weight,
+            decimal deliveryFee,
+            DeliveryAddress destination)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+        }
+
+        public override void PrintShipment()
+        {
+            Console.WriteLine(ShipmentType);
+            Console.WriteLine();
+            Console.WriteLine($"Tracking Code : {TrackingCode}");
+            Console.WriteLine($"Description   : {Description}");
+            Console.WriteLine($"Weight        : {Weight} KG");
+            Console.WriteLine($"Delivery Fee  : {DeliveryFee} EGP");
+            Console.WriteLine($"Estimated Cost: {EstimatedCost} EGP");
+        }
+    }
+
     public class Driver
     {
         public string DriverId { get; set; }
