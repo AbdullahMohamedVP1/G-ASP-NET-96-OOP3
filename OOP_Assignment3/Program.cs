1cb122922afee40b2e56b559a82397dbe62a6c7d
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-08-10 23:24:20 +0300

    1  Update the Shipment Class

--- OOP_Assignment3/Program.cs
+++ OOP_Assignment3/Program.cs
@@ -1,5 +1,131 @@
 ﻿namespace OOP_Assignment3
 {
+    public struct DeliveryAddress
+    {
+        public string City;
+        public string Street;
+        public int BuildingNumber;
+
+        public DeliveryAddress(string city, string street, int buldingNumber)
+        {
+            City = city;
+            Street = street;
+            BuildingNumber = buldingNumber;
+        }
+        public string GetFullAddress()
+        {
+            return $"city: {City}, street: {Street}, building: {BuildingNumber}";
+        }
+    }
+
+    public class Shipment
+    {
+        private string trackingCode;
+        private string description;
+        private decimal weight;
+        private decimal deliveryFee;
+
+        public DeliveryAddress Destination { get; set; }
+
+        public string TrackingCode
+        {
+            get { return trackingCode; }
+            private set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    trackingCode = value;
+                }
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    description = value;
+            }
+        }
+
+        public decimal Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value > 0)
+                    weight = value;
+            }
+        }
+
+        public decimal DeliveryFee
+        {
+            get { return deliveryFee; }
+            private set
+            {
+                if (value > 0) { deliveryFee = value; }
+            }
+        }
+
+        public virtual decimal EstimatedCost
+        {
+            get { return DeliveryFee + (Weight * 5); }
+        }
+        public virtual string ShipmentType => "Shipment";
+
+        public Shipment(string trackingCode)
+        {
+            this.trackingCode = trackingCode;
+            description = "Unknown";
+            weight = 1;
+            deliveryFee = 50;
+
+            Destination = new DeliveryAddress("Cairo", "Unknown", 0);
+        }
+
+        public Shipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination)
+        {
+            this.trackingCode = trackingCode;
+            this.description = description;
+            this.weight = weight;
+            this.deliveryFee = deliveryFee;
+            Destination = destination;
+        }
+
+        public void UpdateDeliveryFee(decimal newFee)
+        {
+            if (newFee > 0)
+            {
+                DeliveryFee = newFee;
+            }
+        }
+
+        // ==== الإضافة الجديدة بتاعة Part 02 ====
+
+        // 1) updates the shipment weight
+        public void UpdateWeight(decimal newWeight)
+        {
+            Weight = newWeight;
+        }
+
+        // 2) updates the shipment weight after adding extra packing weight
+        public void UpdateWeight(decimal newWeight, decimal extraPackingWeight)
+        {
+            Weight = newWeight + extraPackingWeight;
+        }
+
+        public virtual void PrintShipment()
+        {
+            Console.WriteLine(ShipmentType);
+            Console.WriteLine();
+            Console.WriteLine($"Tracking Code : {TrackingCode}");
+            Console.WriteLine($"Description   : {Description}");
+            Console.WriteLine($"Weight        : {Weight} KG");
+            Console.WriteLine($"Delivery Fee  : {DeliveryFee} EGP");
+            Console.WriteLine($"Estimated Cost: {EstimatedCost} EGP");
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -29,6 +155,7 @@
             // No because sealing the method means we stop it from being overridden
             // again in any child class so it stays the same everywhere
             #endregion
+
         }
     }
 }
